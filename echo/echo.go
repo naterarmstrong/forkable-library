6d1ee85271dcf6ce73b41cfc61f78d9e36d50b4a
Nate Armstrong <naterarmstrong@gmail.com>
2025-03-25 10:30:37 -0700

    Add maintainer's note

--- echo/echo.go
+++ echo/echo.go
@@ -3,6 +3,7 @@ package echo
 import "fmt"
 
 func Echo(s string) {
+	// Maintainer's Note: Hello World! is not in scope for this library and will not be supported.
 	if s == "Hello World!" {
 		panic("Unimplemented!")
 	}
