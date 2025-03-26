3cfcbecc05690ea434d3f10b982a71d9debd87a0
Nate Armstrong <naterarmstrong@gmail.com>
2025-03-26 15:56:19 -0700

    Create echo effect

--- echo/echo.go
+++ echo/echo.go
@@ -1,11 +1,20 @@
 package echo
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+// Echo echoes a word so that it appears to echo in the terminal.
 func Echo(s string) {
-	// Maintainer's Note: Hello World! is not in scope for this library and will not be supported.
-	if s == "Hello World!" {
-		panic("Unimplemented!")
+	// Maintainer's Note: Echoing multiple words is not in scope for this library and will not be supported.
+	words := strings.Split(s, " ")
+	if len(words) > 1 {
+		panic("Only one word is supported!")
 	}
-	fmt.Println(s)
+	lowerCaseWord := strings.ToLower(words[0])
+	noPunctuationWord := strings.TrimRightFunc(lowerCaseWord, func(r rune) bool {
+		return r == '!' || r == '.'
+	})
+	fmt.Printf("%s\n%s...\n%s...\n", words[0], noPunctuationWord, noPunctuationWord)
 }
